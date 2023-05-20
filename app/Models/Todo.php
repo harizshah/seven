ae96bceaedd0a2bf783b8c24cf12c92197ce91cf
unknown <harizsharizad@gmail.com>
2023-05-20 16:55:21 +0800

    Todo has Many Steps

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Step;
 
 class Todo extends Model
 {
     protected $fillable = ['title','completed','user_id','description'];
+
+    public function steps()
+    {
+        return $this->hasMany(Step::class);
+    }
 
 //    public function getRouteKeyName()
 //    {
