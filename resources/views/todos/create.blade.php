f02ce137b00b0e9adbc914ccda03b7688e817f5b
unknown <harizsharizad@gmail.com>
2023-05-20 16:03:16 +0800

    Introduction to Laravel LiveWire

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -7,6 +7,8 @@
             <span class="fas fa-arrow-left"/>
         </a>
     </div>
+
+
     <x-alert />
     <form method="post" action="{{route('todo.store')}}" class="py-5">
         @csrf
@@ -16,6 +18,14 @@
         <div class="py-1">
             <textarea name="description" class="p-2 rounded border"
                       placeholder="Description"></textarea>
+        </div>
+
+        <div class="py-2">
+            <div class="flex justify-center pb-4 px-4">
+                <h2 class="text-lg  pb-4">Add steps for task</h2>
+                    <span class="fas fa-plus px-2 py-1 cursor-pointer"/>
+            </div>
+            <input type="text" name="step" class="py-2 px-2 border rounded" placeholder="Describe Step"/>
         </div>
         <div class="py-1">
             <input type="submit" name="Create" class="p-2 border rounded" />
--- resources/views/todos/layout.blade.php
+++ resources/views/todos/layout.blade.php
@@ -6,6 +6,7 @@
     initial scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @livewireStyles
     <title>Todos</title>
 </head>
 <body>
@@ -15,5 +16,7 @@
         @yield('content')
     </div>
 </div>
+
+@livewireScripts
 </body>
 </html>
